68349b13a26d0b08e2c60d9b7b227ac73c239c0e
oreElijah <151626649+oreElijah@users.noreply.github.com>
2025-06-19 20:18:24 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -38,4 +38,4 @@ app.include_router(house_predict_router)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # default to 8000 locally
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
